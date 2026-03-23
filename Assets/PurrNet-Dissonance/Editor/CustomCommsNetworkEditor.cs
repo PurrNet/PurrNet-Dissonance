5a65587606a8cbb8be0f70c1abe94d4055876d04
Quinn Griffin <quinng15@gmail.com>
2026-03-23 09:21:27 -0600

    Implemented dissonance comms network custom editor for the PurrNetCommsNetwork component. Dissonance provides a base class for custom editors for use by different integrations.

---
+++ Assets/PurrNet-Dissonance/Editor/CustomCommsNetworkEditor.cs
@@ -0,0 +1,17 @@
+﻿using Dissonance.Editor;
+using Dissonance.Integrations.PurrNet;
+using PurrNet;
+using UnityEditor;
+
+[CustomEditor(typeof(PurrNetCommsNetwork))]
+public class CustomCommsNetworkEditor
+    : BaseDissonnanceCommsNetworkEditor<
+        PurrNetCommsNetwork,
+        PurrNetServer,
+        PurrNetClient,
+        PlayerID,
+        object,
+        object
+    >
+{
+}
